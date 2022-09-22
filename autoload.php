729103c56f3e52cdf54eced774d36524524b6f08
Samil <agostinhosaml832@gmail.com>
2022-09-22 19:16:24 +0100

    root: create pack autoload files

---
+++ autoload.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @version 2.0
+ * @author Sammy
+ *
+ * @keywords Samils, ils, php framework
+ * -----------------
+ * @namespace Sammy\Packs\Gogue\Component\Base
+ * - Autoload, application dependencies
+ */
+namespace Sammy\Packs\Gogue\Component\Base {
+  use Sammy\Packs\IncludeAll;
+}
